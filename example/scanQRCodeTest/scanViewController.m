64db1a9765e0fc97a281f71bf08eb05a7ee0d4a6
GitHubWanglei <963256255@qq.com>
2016-01-04 14:16:34 +0800

    .
    .

--- example/scanQRCodeTest/scanViewController.m
+++ example/scanQRCodeTest/scanViewController.m
@@ -22,16 +22,22 @@
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor whiteColor];
     
+    //****************************** 添加扫描组件 *************************************
+    
+    //初始化
     CGRect scanView_scope = CGRectMake(self.view.bounds.size.width/2-240/2, self.view.bounds.size.height/2-240/2, 240, 240);
     WLScanQRCodeView *scanView = [WLScanQRCodeView viewWithFrame:self.view.bounds scanScope:scanView_scope];
     [self.view addSubview:scanView];
+    //开启扫描
     [scanView startRunning];
+    //回调
     scanView.scanResultHandle = ^(WLScanQRCodeView *view, NSString *result){
         resultViewController *vc = [[resultViewController alloc] init];
         vc.result = result;
         [self.navigationController pushViewController:vc animated:YES];
     };
     
+    //****************************** 添加扫描组件 *************************************
     self.scanView = scanView;
     
 }
